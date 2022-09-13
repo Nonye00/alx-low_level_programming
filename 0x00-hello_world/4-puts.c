fd56724b2ddbf1d5c00a7632bfbdcca91e560139
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-12 22:15:56 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - prints the text in the put function
+ *main - entry point
  *
- *Description: using the main function
+ *Description: 'using the main function'
  *
  *return:0
  */
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
