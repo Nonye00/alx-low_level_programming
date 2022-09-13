ffabe0e6da45dcbbdb2ba73d22ea11056572f9ad
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-12 22:28:38 -0700

    my fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main: prints a text
+ *main: prints a text
  *
- * return: always 0 (Done)
+ *return: always 0 (Done)
  *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
+main( int argc, char *argv[ ], char *envp[ ] )
+
 
