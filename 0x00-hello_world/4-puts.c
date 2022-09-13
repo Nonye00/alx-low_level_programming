6b7c78c6c8d7e13a9111f71489403367f5cd209a
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-12 21:04:19 -0700

    my fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * main - entry
  *
  * return:0 after printing 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
