9c4ef7b70caddbe231f936d224855c2718bf9679
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-12 17:30:29 -0700

    My fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print the string in the put function
+*
+* Descsription: using the main function
+* prints: Programming is lg a multilingual puzzle'
+* Return: 0
+*/
+int main(void)
+{
+    puts ("\" Programming is like building a multilingual puzzle);
+    return (0);
+}
+^C
+
+
+
