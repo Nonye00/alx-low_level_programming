5374c7b4950dbfd891a17f65dd15c9e53e625079
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-13 17:56:33 -0700

    prints alphabet 3.0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+		int n;
+
+		for (n = 97; n <= 122; ++n)
+		{
+			if (n != 101 && n != 113)
+				{
+					putchar(n);
+				}
+		}										putchar(10);
+		return (0);
+}
