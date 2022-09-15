0c5ba14f18fbf526084d7fe219102336ae8281a8
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-15 12:30:23 -0700

    c commits

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_alphabet-prints alphabets in lowercase
+  */
+void print_alphabet(void)
+{
+	char q;
+	for (q ='a'; q <= 'z'; q++)
+	{
+		_putchar(q);
+	}
+	_putchar('\n');
+}
+
+
+
