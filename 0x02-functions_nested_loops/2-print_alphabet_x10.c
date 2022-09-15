0c8be483c0c797f691fae72cf5c435e20b12d86b
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-15 15:31:28 -0700

    my c commits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
 void print_alphabet_x10(void)
 {
 	char f, q;
- 	for (f = 0; f <= 9; f++);
-
+		for (f = 0; f <= 9; f++);
 	{
-		for (q = 'a'; q <= 'z'; q++);
-
+		for (q = 'a'; q <= 'z'; q++)
 		{
 			_putchar(q);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
+
+
+	
