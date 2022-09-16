ed17d32c2e58b6e95c9002d5ad4f0c1693e95b5f
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-16 03:13:57 -0700

    my second debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h" 
 
 /**
-  *main -test function for positive or negative
-  *return: 0
-  */ 
+ *main -test function for positive or negative
+ *return: 0
+*/ 
 
 int main(void) 
 {
-	int i 
+	int i;
 
-	i = 0
+	i = 0;
 	positive_or_negative(i); 
 
 	return (0);
