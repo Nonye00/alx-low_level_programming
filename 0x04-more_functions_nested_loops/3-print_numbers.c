ff924435ffe7d330622e94d72c662deeb705c0bb
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-17 19:41:11 -0700

    nested loops commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
   * print_numbers - print  0 - 9
-  *
   * Return: void
   */
 
@@ -11,8 +10,7 @@ void print_numbers(void)
 		char a = 0;
 		while (a <= 9)
 		{
-		_putchar(a + '0');
-		a++;
+		_putchar(a + '0'); a++;
 		}
 		_putchar('\n');
 }
