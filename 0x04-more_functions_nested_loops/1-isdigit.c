fb94273b98390badcdcd9d501861bb2a53fa2ac9
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-17 19:04:04 -0700

    nested loops commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * _isdigit - function that checks a digit
+  * @c: return char type
+  * Return: return 1 on success and 0 on fail
+*/
+
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <='9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
