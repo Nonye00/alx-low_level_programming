1db030da83896dc2acddff53df22342a28742659
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-20 16:24:51 -0700

    pointers commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * rev_string - prints a string in reverse
+  * @s: the string to be reversed
+  * Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int length, i, half;
+	char temp;
+
+	for (length = 0; s[length] != '\0'; length++);
+	i = 0;
+	half = length / 2;
+
+	while (half--)
+	{
+		temp = s[length - i - 1];
+		s[length - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
