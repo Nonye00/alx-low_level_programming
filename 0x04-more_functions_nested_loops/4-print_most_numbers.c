bcdbfc98f0b5fb12b6f5b6b39e093908f4c90f29
Chinonyerem Obunma <nonye.obu@gmail.com>
2022-09-23 15:28:41 -0700

    nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,12 @@ void print_most_numbers(void)
 {
 	int a = 0;
 	while (a <= 9)
+
 	{
 		if (a != 2 && a != 4)
 		{
-			_putchar(a + '0'); a++;
+			_putchar(a + '0'); 
+			a++;
 		}
 	}
 		_putchar('\n');
